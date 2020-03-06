b2f6d57e24f3ce22f76b40d0a9d8efbab849cc13
rbjacob101 <rbjacob101@gmail.com>
2020-03-05 22:04:11 -0700

    Working on adding application list tiles

--- src/launcher/CoffeeTableLauncher/CoffeeTableLauncher/MainWindow.xaml.cs
+++ src/launcher/CoffeeTableLauncher/CoffeeTableLauncher/MainWindow.xaml.cs
@@ -21,12 +21,37 @@ namespace CoffeeTableLauncher
 	/// </summary>
 	public partial class MainWindow
 	{
+
 		public MainWindow()
 		{
 			InitializeComponent();
 			ProgressBar.Visibility = Visibility.Hidden;
 			ProgressBar.Value = 100;
+
+			SetupTileGrid();
 		}
 
+		List<TileData> tiles = new List<TileData>();
+
+		/* Test method */
+		private void SetupTileGrid()
+		{
+			for (int i = 0; i < 25; i++)
+				tiles.Add(new TileData { Color = new SolidColorBrush(Colors.Cyan), Id = "1", Title="TestTitle", Description="Description is here." });
+
+			ItemList.ItemsSource = tiles;
+		}
+
+		private void MetroWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+		{
+		}
+	}
+
+	public class TileData
+	{
+		public Brush Color { get; set; }
+		public string Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
 	}
 }
